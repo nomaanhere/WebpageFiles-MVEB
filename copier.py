c8c9bea7f3a5dff4cbe76793c54dec0aa302a45a
Nomaan Parmar <mohamadnomaan.p@somaiya.edu>
2020-03-23 22:14:07 +0530

    Add files via upload

--- copier.py
+++ copier.py
@@ -1,5 +1,12 @@
 import shutil
 
 for i in range(100):
-    shutil.copy2('D:/Img/bottle.xml', 'D:/copies/bottle{}.xml'.format(i))
-    shutil.copy2('D:/Img/bottle.jpg', 'D:/copies/bottle{}.jpg'.format(i))
+    shutil.copy2('D:/Img/annotation.xml', 'D:/copies/annotation{}.xml'.format(i))
+    shutil.copy2('D:/Img/annotation.jpg', 'D:/copies/annotation{}.jpg'.format(i))
+    shutil.copy2('D:/Img/annotation1.xml', 'D:/copies/annotation1{}.xml'.format(i))
+    shutil.copy2('D:/Img/annotation1.jpg', 'D:/copies/annotation1{}.jpg'.format(i))
+    shutil.copy2('D:/Img/annotation2.xml', 'D:/copies/annotation2{}.xml'.format(i))
+    shutil.copy2('D:/Img/annotation2.jpg', 'D:/copies/annotation2{}.jpg'.format(i))
+    shutil.copy2('D:/Img/annotation3.xml', 'D:/copies/annotation3{}.xml'.format(i))
+    shutil.copy2('D:/Img/annotation3.jpg', 'D:/copies/annotation3{}.jpg'.format(i))
+
